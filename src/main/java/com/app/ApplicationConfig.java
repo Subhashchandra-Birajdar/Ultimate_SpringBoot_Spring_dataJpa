513d57d0a1e10ec9c14cae2e3b35023634080f92
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-22 21:40:43 +0530

    Understand use of @Qualifier

--- src/main/java/com/app/ApplicationConfig.java
+++ src/main/java/com/app/ApplicationConfig.java
@@ -1,15 +1,22 @@
 package com.app;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean("myBean")   // <---- Bean naming performed
+    @Bean
+    @Qualifier("bean1")
+    // here we are telling spring spring bean should inject bean1 or bean2 using @Qualifier
     public MyFirstClass myFirstClass() {
         return new MyFirstClass("first bean");
     }
 
-
+    @Qualifier("bean2")
+    @Bean
+    public MyFirstClass mySecondBean() {
+        return new MyFirstClass("Second bean");
+    }
 }
--- src/main/java/com/app/MyFirstService.java
+++ src/main/java/com/app/MyFirstService.java
@@ -1,14 +1,16 @@
 package com.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
     //inject the bean
     private MyFirstClass myFirstClass;
+
     @Autowired // it is optinal on constructor
-    public MyFirstService(MyFirstClass myFirstClass) {
+    public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
         //did constructor based dependency, autowire is optional becuase injecting only one bean
          this.myFirstClass = myFirstClass;
     }
