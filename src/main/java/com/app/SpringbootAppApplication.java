b3a928c40e11f5041a7372c9b4b987f2fea96247
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-25 10:53:56 +0530

    added profile programatically

--- src/main/java/com/app/SpringbootAppApplication.java
+++ src/main/java/com/app/SpringbootAppApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SpringbootAppApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(SpringbootAppApplication.class, args);
-		MyFirstService myFirstClass = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstClass.tellStory());
-		System.out.println(myFirstClass.getCustomProperty());
-		System.out.println(myFirstClass.getCustompropertyfromAnotherField());
-		System.out.println(myFirstClass.getCustomPropertyInt());
-		System.out.println(myFirstClass.getCustompropertyfromAnotherField2());
-
+		var app = new SpringApplication(SpringbootAppApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
+		var ctx=app.run(args);
+		MyFirstService bean = ctx.getBean(MyFirstService.class);
+		System.out.println(bean.tellStory());
+		System.out.println(bean.getCustomProperty());
+		System.out.println(bean.getCustompropertyfromAnotherField());
+		System.out.println(bean.getCustomPropertyInt());
+		System.out.println(bean.getCustompropertyfromAnotherField2());
 	}
 }
+
