1870b46e98c87ce5adcafe258a72a2ff4584397a
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-26 13:35:50 +0530

    Added Application.yaml and clean up code

--- src/main/java/com/app/FirstController.java
+++ src/main/java/com/app/FirstController.java
@@ -12,46 +12,11 @@ public class FirstController {
         return "Hello from my first controller";    // httpStatus return 200 ok
     }      // what is the context path '/' ?
 
-    // http method contain bunch of variable it is present in the @ResponseStatus
-    @GetMapping("/hello")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    // this is nothing but endpoint
-    public String sayHello1(){ //localhost:8080/
-        return "Hello1 from my first controller";
-    }  // httpStatus return 202 ACCEPTED
-
-    // PostMapping means clinet to send some data from client to server
-    @PostMapping("/post")
-    public String post(String message){  // here we need to provide for example
-        return "Request accepted and message is  :"+ message;
-    }
-
-
     @PostMapping("/posting")
     public String postMethod(@RequestBody String message){  // here we need to provide for example
         return "Request accepted and message is  :"+ message;
     }
 
-
-    @PostMapping("/Order_Object")
-    public String orderdisplay(@RequestBody Order order){
-        return " the order is : "+ order.toString();
-    }
-
-    @PostMapping("/save/post-orderRecord")
-    public String save(@RequestBody OrderRecord order){
-        return " the orderRecord : " +order.toString();
-    }
-
-    @GetMapping("/hello/{check}")
-    public String pathVar(@PathVariable("check")String str ){
-        return " the string is : "+str;
-    } // http://localhost:8080/hello/anystring
-
-    @GetMapping("/hello")
-    public String requestParam(@RequestParam("check")String str ){
-        return " the string is : "+str;
-    } // http://localhost:8080/hello?check=anystring
 
 
 
--- src/main/java/com/app/OrderRecord.java
+++
@@ -1,9 +0,0 @@
-package com.app;
-
-public record OrderRecord(
-        int id,
-        String customerName,
-        String productName
-
-) {
-}
